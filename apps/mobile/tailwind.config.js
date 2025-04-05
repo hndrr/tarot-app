8bbe5fd34d71dde03bedf3ceed65a3458ec4cce2
hndrr <yozhik20@gmail.com>
2025-04-05 14:34:42 +0900

    tailwind.config.jsにAppコンポーネントのパスを追加し、nativewindのプリセットを設定。これにより、スタイルの適用範囲が拡大し、アプリのデザインが向上。

--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./App.{js,jsx,ts,tsx}"],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {
       animation: {
